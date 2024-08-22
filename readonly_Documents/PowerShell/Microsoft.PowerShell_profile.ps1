fb0a105e9389312fd0a813ccfb08c92c4e99e3ed
Richard Slater <git@richard-slater.co.uk>
2024-08-22 21:21:42 +0100

    feat(prompt): Tokyo Night Cloud Native

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,16 @@
-oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/RichardSlater/dotfiles/master/dot_config/oh-my-posh/cloud-native.omp.json' | Invoke-Expression
+if (Test-Path "~/.config/oh-my-posh/cloud-native-tokyo-night.omp.json") {
+    oh-my-posh init pwsh --config '~/.config/oh-my-posh/cloud-native-tokyo-night.omp.json' | Invoke-Expression
+} else {
+    oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/RichardSlater/dotfiles/master/dot_config/oh-my-posh/cloud-native-tokyo-night.omp.json' | Invoke-Expression
+}
 
 hugo completion powershell | Out-String | Invoke-Expression
 
 Set-Alias -Name docker -Value podman
 Set-Alias -Name vi -Value nvim
 Set-Alias -Name vim -Value nvim
+Set-Alias -Name ls -Value Get-ChildItem
+Set-Alias -Name ll -Value Get-ChildItem -Force
 
 $env:VISUAL = "nvim"
 $env:EDITOR = "nvim"
