e6e31769134db7a52dbdf0460f542f6ec70aa9c7
Richard Slater <git@richard-slater.co.uk>
2024-09-06 20:51:55 +0100

    fix(bat): add install script

---
+++ scripts/debian/private_executable_install_bat.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BAT_VER="0.24.0"
+TEMP_DIR="/tmp/bat_$(
+  tr -dc a-z0-9 </dev/urandom | head -c 6
+  echo
+)"
+
+echo "Creating directory ${TEMP_DIR}"
+mkdir -p "$TEMP_DIR"
+cd "$TEMP_DIR"
+wget "https://github.com/sharkdp/bat/releases/download/v${BAT_VER}/bat_${BAT_VER}_amd64.deb" -P "$TEMP_DIR"
+sudo dpkg -i ${TEMP_DIR}/bat_${BAT_VER}_amd64.deb
+rm -rf "$TEMP_DIR"
