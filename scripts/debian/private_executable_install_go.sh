3918b570f6e16e7b6d3d7fb4efceccec828de5e3
Richard Slater <git@richard-slater.co.uk>
2024-09-06 20:14:50 +0100

    feat(go): go installer

---
+++ scripts/debian/private_executable_install_go.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+GO_VER="go1.23.1.linux-amd64.tar.gz"
+TEMP_DIR="/tmp/go_$(
+  tr -dc a-z0-9 </dev/urandom | head -c 6
+  echo
+)"
+
+echo "Creating directory ${TEMP_DIR}"
+mkdir -p "$TEMP_DIR"
+cd "$TEMP_DIR"
+wget "https://go.dev/dl/${GO_VER}" -P "$TEMP_DIR"
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf "${TEMP_DIR}/${GO_VER}"
+rm -rf "$TEMP_DIR"
