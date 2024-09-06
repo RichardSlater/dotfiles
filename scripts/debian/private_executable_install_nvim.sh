7008ebba05cbd427357d29afe5cd16d11243b5cd
Richard Slater <git@richard-slater.co.uk>
2024-09-06 14:52:24 +0100

    feat(kali): add nvim install script

---
+++ scripts/debian/private_executable_install_nvim.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Installing dependencies"
+sudo apt-get install ninja-build gettext cmake unzip curl build-essential
+
+TEMP_DIR="/tmp/nvim_$(
+  tr -dc a-z0-9 </dev/urandom | head -c 6
+  echo
+)"
+
+echo "Creating directory ${TEMP_DIR}"
+mkdir -p "$TEMP_DIR"
+cd "$TEMP_DIR"
+git clone https://github.com/neovim/neovim $TEMP_DIR
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
+rm -rf "$TEMP_DIR"
