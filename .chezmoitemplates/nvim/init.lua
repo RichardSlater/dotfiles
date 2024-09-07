38dc36b2266d2269a83e6ebfa2b310551fc4d43e
Richard Slater <git@richard-slater.co.uk>
2024-09-07 17:17:31 +0100

    feat(nvim): much better markdown support

--- .chezmoitemplates/nvim/init.lua
+++ .chezmoitemplates/nvim/init.lua
@@ -53,6 +53,20 @@ require("lazy").setup({
 				"MunifTanjim/nui.nvim",
 			},
 		},
+		{
+			"MeanderingProgrammer/render-markdown.nvim",
+			opts = {},
+			dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
+		},
+		{
+			"iamcco/markdown-preview.nvim",
+			cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+			build = "cd app && yarn install",
+			init = function()
+				vim.g.mkdp_filetypes = { "markdown" }
+			end,
+			ft = { "markdown" },
+		},
 	},
 	install = { colorscheme = { "tokyonight" } },
 	checker = { enabled = true },
@@ -71,6 +85,28 @@ noice.setup({
 	},
 })
 
+-- format buffer on write
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
+})
+
+-- format buffer on demand
+vim.api.nvim_create_user_command("Format", function(args)
+	local range = nil
+	if args.count ~= -1 then
+		local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+		range = {
+			start = { args.line1, 0 },
+			["end"] = { args.line2, end_line:len() },
+		}
+	end
+	require("conform").format({ async = true, lsp_format = "fallback", range = range })
+end, { range = true })
+
+-- configure status line
 require("lualine").setup({
 	options = {
 		icons_enabled = true,
