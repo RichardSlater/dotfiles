655e82db556849de00203397aa5f8e747bbba6bc
Richard Slater <github@richard-slater.co.uk>
2025-02-18 16:01:38 +0000

    fix(): fzf test fix

--- dot_config/dot_fzf.bash
+++ dot_config/dot_fzf.bash
@@ -1,6 +1,6 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/richardslater/.local/fzf/bin* ]]; then
+if [[ ! "$PATH" == *$HOME/.local/fzf/bin* ]]; then
   PATH="${PATH:+${PATH}:}$HOME/.local/fzf/bin"
 fi
 
