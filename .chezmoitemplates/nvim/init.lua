b8f1724dd16cdf56360cf60be236d533c2e20281
Richard Slater <github@richard-slater.co.uk>
2025-11-03 11:16:24 +0000

    fix: update nvim configuration

--- .chezmoitemplates/nvim/init.lua
+++ .chezmoitemplates/nvim/init.lua
@@ -39,6 +39,8 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
 	spec = {
 		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
+		{ "folke/noice.nvim", event = "VeryLazy", opts = { }, dependencies = { } },
+		{ "nvim-treesitter/nvim-treesitter", branch = 'main', lazy = false, build = ":TSUpdate" },
 		{
 			"nvim-lualine/lualine.nvim",
 			dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -84,14 +86,23 @@ require("lazy").setup({
 local custom_tokyonight = require("lualine.themes.tokyonight")
 custom_tokyonight.normal.c.bg = "#1a1b26"
 
-local noice = require("noice")
-noice.setup({
-	routes = {
-		{
-			view = "notify",
-			filter = { event = "msg_showmode" },
-		},
-	},
+require("noice").setup({
+  lsp = {
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+    },
+  },
+  -- you can enable a preset for easier configuration
+  presets = {
+    bottom_search = true, -- use a classic bottom cmdline for search
+    command_palette = true, -- position the cmdline and popupmenu together
+    long_message_to_split = true, -- long messages will be sent to a split
+    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false, -- add a border to hover docs and signature help
+  },
 })
 
 -- format buffer on write
